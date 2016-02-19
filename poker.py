88ef261f19cdcbc3d9939e7cfe6215f5d770f2d8
AaronDemond <aarondemond@dal.ca>
2016-02-19 03:17:11 -0400

    Added docstrings

--- poker.py
+++ poker.py
@@ -1,7 +1,22 @@
 import random
 '''
     Init a new poker obj to setup the game
-    call game.play_round
+    call playSimpleGame to see an example run.
+
+    ex:
+
+        import poker.py
+        my_game = Poker(5,500) // Start a 5 person game, at $500 ea
+        my_game.playSimpleGame()
+
+        then run the above in the python shell
+
+    more to come. For now it will interactively play a game of poker,
+    with the exception being that it WILL show ALL hands. No UI has 
+    been built on top, and this software is intended to be used for
+    teating various game conditions and outcomes. 
+
+
 '''
 class Card:
     def __init__(self, suit, rank): 
@@ -13,6 +28,9 @@ class Card:
 
 class Deck:
     def __init__(self):
+        '''
+            Create a deck, or list, of 52 card objects.
+        '''
         suits = ['H','D','S','C']
         ranks = ['A','2','3','4','5','6','7','8','9','10','J','K','Q']
         cards = []
@@ -40,6 +58,9 @@ class Poker:
         self.starting_stack = starting_stack;
 
     def playSampleGame(self):
+        '''
+            Interactively play a game with the user
+        '''
 
         deck = Deck()
         hands = []
@@ -49,6 +70,7 @@ class Poker:
             hand = deck.drawHand(2)
             hands.append(hand)
 
+        # print hand values. Insert any templating in this area
         for hand in hands:
             print "\nPrinting hand for player\n"
             for card in hand:
@@ -62,8 +84,8 @@ class Poker:
 
             else:
 
-                inp = raw_input('Would you like to flop another card? Hit Enter to continue, type n to stop')
-                if inp in ['y','Y',None,'','yes','YES']:
+                inp = raw_input('Would you like to flop another card? Hit Enter to continue, or hit any other key to stop')
+                if inp in [None]:
                     river = river + deck.drawHand(1)
 
                 else:
