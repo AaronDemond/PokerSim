1b7374be5758636683ce52519ff968e03cb9448e
AaronDemond <aarondemond@dal.ca>
2016-02-19 01:25:51 -0400

    Restructured to OOP

--- poker.py
+++ poker.py
@@ -1,4 +1,8 @@
 import random
+'''
+    Init a new poker obj to setup the game
+    call game.play_round
+'''
 class Card:
     def __init__(self, suit, rank): 
         self.suit = suit
@@ -7,90 +11,85 @@ class Card:
     def __str__(self):
         return "{}".format(self.rank).ljust(2) + self.suit
 
+class Deck:
+    def __init__(self):
+        suits = ['H','D','S','C']
+        ranks = ['A','2','3','4','5','6','7','8','9','10','J','K','Q']
+        cards = []
+
+        for s in suits:
+            for r in ranks:
+                cards.append(Card(s,r))
+        self.cards = cards
+
+    def drawHand(self, ammount):
+        '''
+        Draws a players hand and updates deck
+        '''
+        hand = []
+        for i in range(0,ammount):
+            rand = random.randint(1,len(self.cards) - 1)
+            card_drawn = self.cards[rand]
+            hand.append(card_drawn)
+            self.cards.remove(card_drawn)
+        return hand
+
+class Poker:
+    def __init__(self, num_players, starting_stack):
+        self.num_players = num_players;
+        self.starting_stack = starting_stack;
+
+    def playSampleGame(self):
+
+        deck = Deck()
+        hands = []
+
+        # Draw x hands
+        for x in range(0,self.num_players):
+            hand = deck.drawHand(2)
+            hands.append(hand)
+
+        for hand in hands:
+            print "\nPrinting hand for player\n"
+            for card in hand:
+                print(card)
+
+        # flop up to 5 cards
+        for i in range(0,3):
+
+            if i==0:
+                river = deck.drawHand(3)
+
+            else:
+
+                inp = raw_input('Would you like to flop another card? Hit Enter to continue, type n to stop')
+                if inp in ['y','Y',None,'','yes','YES']:
+                    river = river + deck.drawHand(1)
+
+                else:
+                    print "flip the cards"
+                    break
+
+            print "\nPrinting river\n"
+            for card in river:
+                print(card)
+
+        inp = raw_input("dev_opts? [yn]")
+
+        if inp in [None,'y']:
+                inp = raw_input('Would you like to show deck? [yn]')
+                if inp in [None,'y']:
+                    print "\nPrinting updated deck\n"
+                    for card in deck.cards:
+                        print(card)
 
 
 
-def getDeck():
-    suits = ['H','D','S','C']
-    ranks = ['A','2','3','4','5','6','7','8','9','10','J','K','Q']
-    cards = []
+game = Poker(5,500)
+game.playSampleGame()
 
-    for s in suits:
-        for r in ranks:
-            cards.append(Card(s,r))
-
-    return cards
-
-def drawHand(ammount, deck):
-    ''' Returns a deck of ammount card objects '''
-    #deck should be dict of cards
-    #ammount should be int
-
-    hand = []
-    for i in range(0,ammount):
-        rand = random.randint(1,len(deck) - 1)
-        hand.append(deck[rand])
-
-    return hand
-
-
-def removeFromDeck(cards, deck):
-    ''' removes all cards in the cards dict from the deck dict '''
-    for c in cards:
-        if c in deck:
-            deck.remove(c)
-
-    return deck
 
     
-deck = getDeck()#get new deck
-hands = []
-
-#Get a deck, clear the table
-
-for x in range(0,5):
-    hand = drawHand(2,deck) #draw a hand from deck
-    hands.append(hand)
-    deck = removeFromDeck(hand, deck)  #update deck by removing drawn cards
-
-for hand in hands:
-    print "\nPrinting hand for player\n"
-    for card in hand:
-        print(card)
-
-for i in range(0,4):
-    if i==0:
-        river = drawHand(3, deck)
-
-
-
-    else:
-
-        inp = raw_input('Would you like to flop another card? [fh]')
-        if inp in ['y','Y',None,'','yes','YES']:
-            river = river + drawHand(1,deck)
-
-        else:
-            print "flip the cards"
-            break
-
-    print "\nPrinting river\n"
-    for card in river:
-        print(card)
-
-
-
-    deck = removeFromDeck(river, deck)
-
-
-inp = raw_input("dev_opts? [yn]")
-
-if inp in [None,'y']:
-        inp = raw_input('Would you like to show deck? [yn]')
-        if inp in [None,'y']:
-            print "\nPrinting updated deck\n"
-            for card in deck:
-                print(card)
 
 
     
